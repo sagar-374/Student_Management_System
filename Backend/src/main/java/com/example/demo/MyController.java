5380fba306aab65cbfe84685d59fc523b19bd5c1
sagar-374 <sagarkate374@gmail.com>
2026-05-03 00:09:48 +0530

    backend deploy

--- Backend/src/main/java/com/example/demo/MyController.java
+++ Backend/src/main/java/com/example/demo/MyController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin(origins = "https://student-management-system-one-fawn.vercel.app")
 public class MyController {
 
     @Autowired
