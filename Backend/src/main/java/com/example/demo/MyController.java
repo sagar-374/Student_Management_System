a353a279f9f5a7d7e7a19fb9c99d2d23f4ac9278
sagar-374 <sagarkate374@gmail.com>
2026-05-03 11:27:24 +0530

    backend deploy

--- Backend/src/main/java/com/example/demo/MyController.java
+++ Backend/src/main/java/com/example/demo/MyController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "https://student-management-system-one-fawn.vercel.app/")
 @RestController
 public class MyController {
 
